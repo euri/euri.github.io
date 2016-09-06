3f23ecf03354dcbb3403c1ba5cb6d87cbafc2b28
Euri <euri@beyondeternal.com>
2016-09-06 10:41:46 +0800

    removed old files

--- scripts/snow.js
+++ scripts/snow.js
@@ -9,13 +9,13 @@
 
 function __ShowSnow(settings)
 {
-    
+
     var snowsrc = settings.SnowImage;
     var no = settings.Quantity;
-    
+
     var dx, xp, yp;    // coordinate and position variables
     var am, stx, sty;  // amplitude and step variables
-    var i; 
+    var i;
 
     var doc_width = $(window).width() - 10;
     var doc_height = $(window).height();
@@ -27,16 +27,16 @@ function __ShowSnow(settings)
     stx = [];
     sty = [];
     flakes = [];
-    for (i = 0; i < no; ++i) 
+    for (i = 0; i < no; ++i)
     {
         dx[i] = 0;                        // set coordinate variables
-        xp[i] = Math.random()*(doc_width-50);  // set position variables
-        yp[i] = Math.random()*doc_height;
-        am[i] = Math.random()*20;         // set amplitude variables
-        stx[i] = 0.02 + Math.random()/10; // set step variables
+        xp[i] = Math.random() * (doc_width - 50);  // set position variables
+        yp[i] = Math.random() * doc_height;
+        am[i] = Math.random() * 20;         // set amplitude variables
+        stx[i] = 0.02 + Math.random() / 10; // set step variables
         sty[i] = 0.7 + Math.random();     // set step variables
-        
-        var randno = Math.floor(snowsrc.length*Math.random());
+
+        var randno = Math.floor(snowsrc.length * Math.random());
         var randsnowsrc = snowsrc[randno];
 
         var flake = $("<div />");
@@ -57,20 +57,20 @@ function __ShowSnow(settings)
     }
 
     var animateSnow;
-    animateSnow = function() 
-    {  
-        for (i = 0; i < no; ++ i) 
+    animateSnow = function ()
+    {
+        for (i = 0; i < no; ++i)
         {
             // iterate for every dot
             yp[i] += sty[i];
-            if (yp[i] > doc_height - 50) 
+            if (yp[i] > doc_height - 50)
             {
                 xp[i] = Math.random() * (doc_width - am[i] - 30);
                 yp[i] = 0;
                 stx[i] = 0.02 + Math.random() / 10;
                 sty[i] = 0.7 + Math.random();
             }
-      
+
             dx[i] += stx[i];
             flakes[i].css("top", yp[i] + "px");
             flakes[i].css("left", (xp[i] + am[i] * Math.sin(dx[i])) + "px");
@@ -79,33 +79,33 @@ function __ShowSnow(settings)
         snowtimer = setTimeout(animateSnow, 10);
     };
 
-	function hidesnow()
+    function hidesnow()
     {
-		if(window.snowtimer)
+        if (window.snowtimer)
             clearTimeout(snowtimer)
 
         for (i = 0; i < no; i++)
             flakes[i].hide();
-	}
-		
+    }
+
     animateSnow();
-	if (settings.HideSnowTime > 0)
-    	setTimeout(hidesnow, settings.HideSnowTime * 1000)
+    if (settings.HideSnowTime > 0)
+        setTimeout(hidesnow, settings.HideSnowTime * 1000)
 }
 
-(function($) {
-    $.fn.snow = function(options) {
-  
-    var settings = $.extend({
-            SnowImage:      undefined,
-            Quantity:       7,
-            HideSnowTime:   0
+(function ($) {
+    $.fn.snow = function (options) {
+
+        var settings = $.extend({
+            SnowImage: undefined,
+            Quantity: 7,
+            HideSnowTime: 0
         }, options);
 
-    __ShowSnow(settings);
+        __ShowSnow(settings);
 
-    return this;
-  }
+        return this;
+    }
 
 })(jQuery);
 
